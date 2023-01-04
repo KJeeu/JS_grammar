988c52946c3b34b9553939d23c56a2dda5d8e987
KJeeu <116357790+KJeeu@users.noreply.github.com>
2023-01-04 23:53:28 +0900

    오브젝트, 배열

---
+++ array.js
@@ -0,0 +1,65 @@
+//배열
+
+//1. 배열 선언
+const arr1 = new Array();
+const arr2 = [1,2]; //0,1 인덱스를 가진 배열 생성
+
+//2. 인덱스
+const fruits = ['apple','banana'];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[0]);
+console.log(fruits.length - 1); //마지막 인덱스 가져오기
+
+console.clear();
+//3. 배열 안 전체 출력
+//for
+for (let i=0; i<fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+//for of
+for(let fruit of fruits) {
+    console.log(fruit);
+}
+
+//forEach
+fruits.forEach((fruit, index) => console.log(fruit, index));
+
+//4. addtion, deletion, copy
+//추가 push
+fruits.push('peach');
+
+//빼기 pop
+fruits.pop();
+
+//앞에 넣기 unshift
+fruits.unshift('lemon');
+
+//앞에 빼기 shift
+fruits.shift();
+
+//shift, unshift는 pop,push보다 느림 -> 기존의 데이터들이 움직여야하기 때문에
+
+//5. splice
+fruits.splice(1, 1, 'strawberry')//시작인덱스, 수, 지운 위치에 추가
+console.log(fruits);
+
+//6. concat 
+const number = ['1', '2', '3'];
+const number2 = ['4','5'];
+const numberTotal = number.concat(number2);
+console.log(numberTotal);
+
+console.clear();
+//7. search (indexOf, includes)
+console.log(numberTotal.indexOf('3')); //->2
+console.log(numberTotal.indexOf('7')); //->-1
+
+console.log(numberTotal.includes('7')); //->flase
+
+console.clear();
+//lastIndexOf
+numberTotal.push('1');
+console.log(numberTotal.indexOf('1')); //-> 0 (첫번째 해당 인덱스)
+console.log(numberTotal.lastIndexOf('1')); //->5 (마지막 해당 인덱스)
---
+++ object.js
@@ -0,0 +1,58 @@
+//object = {key : value};
+const jeeu = {};
+const jeeu2 = new Object();
+
+const test = {name: 'jeeu', age:20};
+console.log(test);
+
+test.hasJob = true;
+console.log(test);
+
+delete test.hasJob;
+console.log(test);
+
+
+console.log(test.name);
+console.log(test['name']); //정확히 어떤 key가 필요한지 모를 때 사용(예를 들어 사용자에게 input받는 상황)
+
+
+const person1 = {name : 'a', age: 1};
+const person2 = {name : 'b', age: 2};
+const person3 = new Person('c', 3);
+console.log(person3);
+
+function Person(name, age){
+    this.name = name;
+    this.age = age;    
+}
+
+// key in obj?
+console.log('name' in test); //-> true
+console.log('random' in test); //-?false
+
+//for..in vs for..of
+for (key in test) {
+    console.log(key);
+}
+
+const array = [1,2,3,4,5];
+for (key of array){
+    console.log(key);
+}
+
+//cloning
+const user = {name:'user', age: 33};
+const user2 = user;
+
+user2.name = 'coder';
+console.log(user);
+
+//copy
+const user3 = Object.assign({}, user);
+console.log(user3);
+
+//example
+const ex1 = {color:'red'};
+const ex2 = {color:'blue', size:'big'};
+const mixed = Object.assign({}, ex1, ex2); //-> blue, big (뒤가 앞을 덮음)
+console.log(mixed);
